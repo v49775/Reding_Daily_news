b0b4129190be9c498a13f464c622b659f0d5aa94
Vinayak <waghvinayak16@gmail.com>
2022-03-08 00:22:05 +0530

    first commit

---
+++ 55_exercise_09.py
@@ -0,0 +1,44 @@
+import requests
+
+
+def NewsFromBBC():
+    # BBC news api
+    # following query parameters are used
+    # source, sortBy and apiKey
+    query_params = {
+        "source": "bbc-news",
+        "sortBy": "top",
+        "apiKey": "API_KEY"
+    }
+    main_url = " https://newsapi.org/v1/articles"
+
+    # fetching data in json format
+    res = requests.get(main_url, params=query_params)
+    open_bbc_page = res.json()
+
+    # getting all articles in a string article
+    article = open_bbc_page["articles"]
+
+    # empty list which will
+    # contain all trending news
+    results = []
+
+    for ar in article:
+        results.append(ar["title"])
+
+    for i in range(len(results)):
+        # printing all trending news
+        print(i + 1, results[i])
+
+    # to read the news out loud for us
+    from win32com.client import Dispatch
+    speak = Dispatch("SAPI.Spvoice")
+
+    speak.Speak("news start now")
+    speak.Speak(results)
+
+
+# Driver Code
+if __name__ == '__main__':
+    # function call
+    NewsFromBBC()
